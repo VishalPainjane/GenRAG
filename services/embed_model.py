fcf1c4224e34089b709754ecdb2baa190cfc0dd1
Vishal Painjane <138028102+VishalPainjane@users.noreply.github.com>
2024-12-05 18:35:34 +0530

    Updated the Embedding generation with batch processing (#14)
    * Updated the Embedding generation with batch processing
    
    * updating requrements
    
    * Updated embedding

--- services/embed_model.py
+++ services/embed_model.py
@@ -2,8 +2,13 @@ from sentence_transformers import SentenceTransformer
 from tqdm.auto import tqdm
 import torch
 
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cpu"
+
+batch_size = 32
+
+if device == "cuda":
+    batch_size = 128
 
 print(f"Loading the model on device: {device}")
 embedding_model = SentenceTransformer(model_name_or_path="all-mpnet-base-v2", 
@@ -14,13 +19,24 @@ embedding_model.to(device)
 print("Model loaded successfully")
 
 def embed_text(pages_and_chunks_over_min_token_len):
+    global batch_size
 
-    print("Embedding text chunks")
-    for item in tqdm(pages_and_chunks_over_min_token_len):
-        item["embedding"] = embedding_model.encode(item["sentence_chunk"])
+    print("Embedding text chunks in batches")
+    print(f"Batch size: {batch_size}")
+    all_chunks = [item["sentence_chunk"] for item in pages_and_chunks_over_min_token_len]
 
-    
+    for i in tqdm(range(0, len(all_chunks), batch_size), desc="Processing batches"):
+        batch = all_chunks[i:i + batch_size]
+        
+        embeddings = embedding_model.encode(
+            batch, 
+            device=device, 
+            convert_to_tensor=True,  
+            show_progress_bar=False 
+        )
+        
+        for j, emb in enumerate(embeddings):
+            pages_and_chunks_over_min_token_len[i + j]["embedding"] = emb
 
-    
-
-
+    print("Embedding complete")
+    return pages_and_chunks_over_min_token_len, None
